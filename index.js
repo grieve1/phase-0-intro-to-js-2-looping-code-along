110395d268bf3d15c4aad41e638f71b0167fc0dc
drake bishop <drakemx85@gmail.com>
2023-01-23 04:43:35 -0600

    commit tree df7c706c8e0c47bdc4c4a39d69c1667aeaf09705

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solutions in this file
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(names, event) {
+   return  names.map(name=> `Thank you, ${name}, for the wonderful ${event} gift!`)
+}
+
+console.log(writeCards(names, "surprise"))
+
+function countDown() {
+    for (let countup = 0; countup < 11; countup++) {
+        console.log(countup);
+      }
+}
+
+
+
+
+
+
+
+
+
+
+
+// writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+// const writeCards = ["Guadalupe", "Ollie", "Aki"];
+// const gift = "surprise";
+// const messages = writeCards(names, gift);
+// console.log(messages);
+
