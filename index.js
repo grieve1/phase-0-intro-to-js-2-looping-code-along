cfbcb4a448eb59cf6ac436bbdb93657aece0cd09
drake bishop <drakemx85@gmail.com>
2023-03-05 16:08:38 -0600

    worked on while loops in functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cardNames = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(cardNames) {
+    return [
+        "Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!"
+    ]
+}
+
+function countDown() {
+    let number = 10;
+    
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+}
